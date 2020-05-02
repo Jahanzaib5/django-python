6cf8c4e73be307d597df50c33c829ef8c3496aed
Jahanzaib5 <50393893+Jahanzaib5@users.noreply.github.com>
2020-05-02 21:26:37 +0600

    working

--- airline/Flights/models.py
+++ airline/Flights/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Airports(models.Model):
+	code = models.CharField(max_length=3)
+	city = models.CharField(max_length=64)
+
+	def _str_(self):
+		return f"{self.code}--{self.city}"
+
+
+
+class Flights(models.Model):
+	origin = models.CharField(max_length=64)
+	destination = models.CharField(max_length=64)
+	duration = models.IntegerField()
+
+	def __str__(self):
+		return f"{self.id} - {self.origin} to {self.destination}"
